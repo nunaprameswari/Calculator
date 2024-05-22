386e203af100400832a18534a3cbf2d8badc1e2a
nindiappc <nindi.nindia44@gmail.com>
2024-05-22 22:41:46 +0700

    Editing Peletakan Angka

--- App.jsx
+++ App.jsx
@@ -5,6 +5,18 @@ const App = () => {
   return (
     <View style={styles.Background}>
       <StatusBar style={styles.Background} />
+
+      <View style={styles.Simbol}>
+        <View style={styles.PnAngka}>
+          <Text style={styles.Angka}>1</Text>
+        </View>
+        <View style={styles.PnAngka}>
+          <Text style={styles.Angka}>2</Text>
+        </View>
+        <View style={styles.PnAngka}>
+          <Text style={styles.Angka}>3</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -15,5 +27,18 @@ const styles = StyleSheet.create({
   Background: {
     backgroundColor: '#000',
     flex: 1,
+  },
+  Simbol: {
+    flexDirection: 'row',
+  },
+  PnAngka: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Angka: {
+    color: 'white',
+    fontSize: 25,
+    padding: 10,
   },
 });
